645fc81dd0453b82807db9380ba72bae26028fbb
TimBauchspieß <t.bauchspiess@ampeersenergy.de>
2020-11-04 11:52:03 +0100

    refactor(action): 🛠 entrypoint script adjustments for custom use case
    * removed unused operations
    * renamed `source_dir` -> `source_path`
    * introduced `destination_path`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-if [ -z "$INPUT_SOURCE_DIR" ]; then
-  echo "source directory is not set. Quitting."
-  exit 1
+if [ -z "$INPUT_SOURCE_PATH" ]; then
+    2>&1 echo "source directory is not set. Quitting."
+    exit 1
+fi
+
+if [ -z "$INPUT_DESTINATION_PATH" ]; then
+    2>&1 echo "destination directory is not set. Defaulting to '/'."
+    INPUT_DESTINATION_PATH="/"
 fi
 
 if [ -z "$INPUT_CONTAINER_NAME" ]; then
-  echo "storage account container name is not set. Quitting."
-  exit 1
+    2>&1 echo "storage account container name is not set. Quitting."
+    exit 1
 fi
 
 if [ -z "$INPUT_CONNECTION_STRING" ]; then
-  echo "storage account connection string is not set. Quitting."
-  exit 1
+    2>&1 echo "storage account connection string is not set. Quitting."
+    exit 1
 fi
 
-EXTRA_ARGS=${INPUT_EXTRA_ARGS:""}
+EXTRA_ARGS=${INPUT_EXTRA_ARGS:-}
 
-VERB="upload-batch"
-CONTAINER_NAME_FLAG="--destination"
-if [ -z "$INPUT_SYNC" ]; then
-  VERB="sync"
-  CONTAINER_NAME_FLAG="--container"
-fi
-az storage blob ${VERB} --connection-string ${INPUT_CONNECTION_STRING} --source ${INPUT_SOURCE_DIR} ${CONTAINER_NAME_FLAG} ${INPUT_CONTAINER_NAME} ${EXTRA_ARGS}
+# See <https://docs.microsoft.com/en-us/cli/azure/storage/blob?view=azure-cli-latest> for details
+az storage blob upload-batch \
+    --connection-string "${INPUT_CONNECTION_STRING}" \
+    --source "${INPUT_SOURCE_PATH}" \
+    --destination "${INPUT_CONTAINER_NAME}" \
+    --destination-path "${INPUT_DESTINATION_PATH}" \
+    "${EXTRA_ARGS}"
